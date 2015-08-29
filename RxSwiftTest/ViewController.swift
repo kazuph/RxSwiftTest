9a70ad53f8e64194f917c2b90597e77f8726d046
Kazuhiro Homma <kazu.homma@gmail.com>
2015-08-29 18:26:27 +0900

    Initial Commit

---
+++ RxSwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftTest
+//
+//  Created by 本間和弘 on 2015/08/29.
+//  Copyright (c) 2015年 Kazuhiro Homma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
